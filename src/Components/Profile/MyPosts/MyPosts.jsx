7b8d962621d53108e4059040c98d92232f00994e
Vitalij <r.vitalik.skuratov@gmail.com>
2021-09-21 19:58:17 +0300

    Added props.

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -9,9 +9,9 @@ function MyPosts() {
                 <textarea>New Post</textarea>
                 <button type={"submit"}>Submit</button>
             </div>
-            <Post />
-            <Post />
-            <Post />
+            <Post message='Hi how are you?' likes={10}/>
+            <Post message="It's my firs project" likes={45}/>
+            <Post message="Goodbye" likes={0}/>
         </div>
     )
 }
--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import s from './Post.module.css';
 
-function Post() {
+function Post(props) {
     return (
         <div className={s.item}>
             <img className={s.icon} src="https://decider.com/wp-content/uploads/2020/12/AVA-NETFLIX-REVIEW.jpg?quality=90&strip=all" alt="img"/>
-            Post<br/>
-            <span>like</span>
+            {props.message}
+            <br/>
+            <span>like: {props.likes}</span>
         </div>
     )
 }
