a7d93c1b30ab9009f4ae413bd1132d850ba7a764
Vitalij <r.vitalik.skuratov@gmail.com>
2021-09-23 01:03:05 +0300

    Added structure for Dialogs page.

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
   return (
     <div>
-        Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/1' activeClassName={s.active}>Valera</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2' activeClassName={s.active}>Ihor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3' activeClassName={s.active}>Olha</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Ok</div>
+            </div>
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import Header from "./Components/Header/Header";
 import NavBar from "./Components/NavBar/NavBar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
 import Music from "./Components/Music/Music";
 import News from "./Components/News/News";
 import Settings from "./Components/Settings/Settings";
+import {BrowserRouter, Route} from "react-router-dom";
 
 class App extends React.Component {
     render() {
