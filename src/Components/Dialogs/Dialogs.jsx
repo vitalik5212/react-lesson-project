26f2a4e618dd0e616e17ff8b4432fdce6a108fc8
Vitalij <r.vitalik.skuratov@gmail.com>
2021-09-24 00:14:41 +0300

    Slice UI and BLL.

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -4,6 +4,22 @@ import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
 
+    let dialogs = [
+        {id: 1, name: 'Valera'},
+        {id: 2, name: 'Ihor'},
+        {id: 3, name: 'Stepan'},
+        {id: 4, name: 'Vadim'},
+        {id: 5, name: 'Ira'},
+    ];
+
+    let messages = [
+        {id: 1, text: 'Hi'},
+        {id: 2, text: 'Hello'},
+        {id: 3, text: 'How are you?'},
+        {id: 4, text: 'Fine'},
+        {id: 5, text: 'Good'},
+    ]
+
     const DialogItem = (props) => {
 
         let path = '/dialogs/' + props.id;
@@ -23,18 +39,12 @@ const Dialogs = (props) => {
     <div>
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name='Valera' id='1' />
-                <DialogItem name='Ihor' id='2' />
-                <DialogItem name='Stepan' id='3' />
-                <DialogItem name='Vadim' id='4' />
-                <DialogItem name='Ira' id='5' />
+                <DialogItem name={dialogs[0].name} id={dialogs[0].id} />
+                <DialogItem name={dialogs[1].name} id={dialogs[0].id} />
             </div>
             <div className={s.messages}>
-                <Message text='Hi' />
-                <Message text='Hello' />
-                <Message text='How are you?' />
-                <Message text='Fine' />
-                <Message text='Good' />
+                <Message text={messages[0].text} />
+                <Message text={messages[1].text} />
             </div>
         </div>
     </div>
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 function MyPosts() {
+
+    let posts =  [
+        {id: 1, message: 'Hi how are you?', likesCount: 16},
+        {id: 2, message: 'It\'s my firs project', likesCount: 45},
+        {id: 3, message: 'Goodbye', likesCount: 0},
+    ];
+
     return (
         <div className={s.myPosts}>
             <div>
@@ -12,9 +19,9 @@ function MyPosts() {
                 </div>
                 <button type={"submit"}>Submit</button>
             </div>
-            <Post message='Hi how are you?' likes={10}/>
-            <Post message="It's my firs project" likes={45}/>
-            <Post message="Goodbye" likes={0}/>
+            <Post message={posts[0].message} likes={posts[0].likesCount}/>
+            <Post message={posts[1].message} likes={posts[1].likesCount}/>
+            <Post message={posts[2].message} likes={posts[2].likesCount}/>
         </div>
     )
 }
