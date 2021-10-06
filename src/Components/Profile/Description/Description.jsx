f876233270406a2f139ccec5acdc4018982a1d62
Vitalij <r.vitalik.skuratov@gmail.com>
2021-10-06 22:25:54 +0300

    Fixed border radius into Profile module.

--- src/Components/Profile/Description/Description.jsx
+++ src/Components/Profile/Description/Description.jsx
@@ -4,7 +4,7 @@ import s from './Description.module.css';
 const Description = (props) => {
     return (
         <div>
-            <div>
+            <div className={s.mainImg}>
                 <img alt="img"
                      src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"/>
             </div>
