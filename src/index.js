aed5e765c92cbd2be10924bc44c126905beebeb0
Vitalij <r.vitalik.skuratov@gmail.com>
2021-10-09 20:17:52 +0300

    Deleted cyclic dependencies. Deleted render.js.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import {renderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {addPost, changeNewPostText, subscribe} from "./redux/state";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import React from "react";
+import './index.css';
+
+let renderEntireTree = (state) => {
+    ReactDOM.render(
+        <App state={state}
+             addPost={addPost}
+             changeNewPostText={changeNewPostText}
+        />,
+        document.getElementById('root')
+    );
+}
 
 renderEntireTree(state);
 
+subscribe(renderEntireTree);
